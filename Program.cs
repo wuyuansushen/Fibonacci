782be39b314dce02b73840de50fd8c572f650a1d
wuyuansushen <819070864@qq.com>
2021-03-07 00:11:54 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            int Result=FibonacciSum(input);
+            Console.WriteLine($"{Result}");
+        }
+
+        static int FibonacciSum(int i)
+        {
+            if (i == 1)
+                return 1;
+            else if (i == 0)
+                return 0;
+            else
+                return FibonacciSum(i - 1)+FibonacciSum(i-2);
+        }
+    }
+}
